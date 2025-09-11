c016e16698446d12bc39377a559d0fee7f0f6856
Erik Jishammar <erikjishammar@hotmail.com>
2025-09-11 17:18:23 +0200

    Feat: Finished edit button

--- main-project/backend/server.js
+++ main-project/backend/server.js
@@ -1,54 +1,64 @@
 import express from "express";
 import cors from "cors";
-import {MongoClient} from "mongodb";
+import { MongoClient } from "mongodb";
 
 const app = express();
 const port = 3000;
 // Middleware
 app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT", "DELETE"] }));
-app.use(express.json()); 
-
+app.use(express.json());
 
 (async () => {
-    try{
-
-const uri = "mongodb://127.0.0.1:27017" // MongoDB setup
-const client = new MongoClient(uri); 
-await client.connect();
-const db = client.db("trainingApp");
-const collection = db.collection("exercises"); 
-
-app.get('/exercises', async (req, res) => {
   try {
-    const exercises = await collection.find({}).toArray();
-    res.json(exercises);
-  } catch (error) {
-    console.log("Problem vid hämtning från databasen", error);
-    res.status(500).json({ error: 'Kunde inte hämta övningar' });
-  }
-});
+    const uri = "mongodb://127.0.0.1:27017"; // MongoDB setup
+    const client = new MongoClient(uri);
+    await client.connect();
+    const db = client.db("trainingApp");
+    const collection = db.collection("exercises");
 
-app.post('/form', async (req, res) => {
-    const newExercise = req.body; // Hämta objekt från frontend
-    console.log('Data mottagen', newExercise);
-
-    try {
-    const result = await collection.insertOne(newExercise);
-    res.json({ 
-        message: 'Övning mottagen', 
-        exercise: { ...newExercise, _id: result.insertedId } 
+    app.get("/exercises", async (req, res) => {
+      try {
+        const exercises = await collection.find({}).toArray();
+        res.json(exercises);
+      } catch (error) {
+        console.log("Problem vid hämtning från databasen", error);
+        res.status(500).json({ error: "Kunde inte hämta övningar" });
+      }
     });
-} catch (error) {
-    console.log("Problem vid sparande i databasen", error);
-    res.status(500).json({ error: 'Kunde inte spara övningen' });
-}
-});
 
-app.listen(port, () => {
-    console.log(`Servern kör på http://localhost:${port}`);
-});
-} catch (error) {
+    app.post("/form", async (req, res) => {
+      const newExercise = req.body; // Hämta objekt från frontend
+      console.log("Data mottagen", newExercise);
+
+      try {
+        const result = await collection.insertOne(newExercise);
+        res.json({
+          message: "Övning mottagen",
+          exercise: { ...newExercise, _id: result.insertedId },
+        });
+      } catch (error) {
+        console.log("Problem vid sparande i databasen", error);
+        res.status(500).json({ error: "Kunde inte spara övningen" });
+      }
+    });
+
+    app.put("exercises/:id", async(req,res) => {
+      const { id } = req.params;
+      const updateExercise = req.body;
+      await collection.updateOne({ ID: Number(id) }, {$set: updateExercise});
+      res.json({messege: "uppdaterad"})
+    })
+
+    app.delete("exercises/;id", async(req, res) => {
+      const { id } = req.params;
+      await collection.deleteOne({ID: Number(id) }); 
+      res.json ({messege:"raderad"});
+    })
+
+    app.listen(port, () => {
+      console.log(`Servern kör på http://localhost:${port}`);
+    });
+  } catch (error) {
     console.error("Misslyckades med att koppla till mongodb", err);
-    
-}
+  }
 })();
--- main-project/frontend/src/main.js
+++ main-project/frontend/src/main.js
@@ -1,14 +1,13 @@
-const form = document.getElementById("log-form"); 
-const logList = document.getElementById("log-list"); 
+const form = document.getElementById("log-form");
+const logList = document.getElementById("log-list");
 
-const stats = document.getElementById("stats-container"); 
+const stats = document.getElementById("stats-container");
 const passGenerator = document.getElementById("create-pass-generator");
 
 let logData = []; // skapa array/lista
 let currentEditId = null;
 
-
-document.addEventListener('DOMContentLoaded', async () => {
+document.addEventListener("DOMContentLoaded", async () => {
   try {
     const response = await fetch("http://localhost:3000/exercises"); // GET förfrågan
     if (response.ok) {
@@ -20,74 +19,92 @@ document.addEventListener('DOMContentLoaded', async () => {
   }
 });
 
+form.addEventListener("submit", async (event) => {
+  event.preventDefault(); // Hindra sidan från att ladda om
 
-form.addEventListener("submit", async (event) => {  
-    event.preventDefault(); // Hindra sidan från att ladda om
+  // Hämta data från formuläret och skapa objekt
+  const newExercise = {
+    ID: Date.now(), // Unik ID för redigering och borttagning
+    datum: form.date.value,
+    övning: form.exercise.value,
+    set: form.set.value,
+    reps: form.reps.value,
+    vikt: form.weight.value,
+    kommentar: form.comment.value,
+  };
 
-    // Hämta data från formuläret och skapa objekt
-    const newExercise = {
-        ID: Date.now(), // Unik ID för redigering och borttagning
-        datum: form.date.value,
-        övning: form.exercise.value,
-        set: form.set.value,
-        reps: form.reps.value,
-        vikt: form.weight.value,
-        kommentar: form.comment.value
-    };
-     /* if (currentEditId) {
-        // sätta i "edit-läge" -> med find metod hitta rätt ID och köra en PUT(fetch) 
-        // Else "nytt läge" - POST fetch (lägga in funktioner för anrop?)
+  let responseData;
+  if (currentEditId) {
+    const response = await fetch(
+      `http://localhost:3000/exercises/${currentEditId}`,
+      {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newExercise),
+      }
+    );
+    responseData = await response.json();
+    console.log("övning uppdaterad");
+
+    for (let i = 0; 1 < logData.length; i++) {
+      if (logData[i].ID === currentEditId) {
+        logData[i].datum = newExercise.datum;
+        logData[i].övning = newExercise.övning;
+        logData[i].set = newExercise.set;
+        logData[i].reps = newExercise.reps;
+        logData[i].vikt = newExercise.vikt;
+        logData[i].kommentar = newExercise.kommentar;
+        break;
+      }
     }
-        */
-    
-    try {
-        const result = await fetch("http://localhost:3000/form", {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(newExercise)
-        });
+  } else {
+    const result = await fetch("http://localhost:3000/form", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newExercise),
+    });
 
-        const responseData = await result.json(); // läs svaret från express-servern
-        console.log("Nytt pass sparat:", responseData);
+    const responseData = await result.json(); // läs svaret från express-servern
+    console.log("Nytt pass sparat:", responseData);
 
-       logData.push(responseData.exercise || responseData);
-        renderLogList();
+    logData.push(responseData.exercise || responseData);
 
-        // Nollställ formuläret
-        form.reset();
-    } catch (error) {
-        console.error(error);
-    }
+    renderLogList();
+    form.reset();
+    currentEditId = null; // återställ "edit-läge"
+  }
 });
 
 // Funktion för att rendera lista
 function renderLogList() {
-    logList.innerHTML = ""; // Tömmer listan
-    console.log("Alla pass sparade:", logData);
+  logList.innerHTML = ""; // Tömmer listan
+  console.log("Alla pass sparade:", logData);
 
-    logData.forEach(exercise => {
-        const li = document.createElement("li"); // Skapa <li> för varje övning/pass
-        li.textContent = `${exercise.datum} - ${exercise.övning} (${exercise.set}x${exercise.reps}) - ${exercise.vikt}kg${exercise.kommentar ? ": " + exercise.kommentar : ""}`;
-        
-        const editBtn = document.createElement("button"); 
-        editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
-        editBtn.addEventListener("click", () => {
-            
-            currentEditId = exercise.ID // ID  för "edit läge"
-            form.date.value = exercise.datum; 
-            form.exercise.value = exercise.övning;
-            form.set.value = exercise.set; 
-            form.reps.value = exercise.reps; 
-            form.weight.value = exercise.vikt; 
-            form.comment.value = exercise.kommentar || ""; 
+  logData.forEach((exercise) => {
+    const li = document.createElement("li"); // Skapa <li> för varje övning/pass
+    li.textContent = `${exercise.datum} - ${exercise.övning} (${exercise.set}x${
+      exercise.reps
+    }) - ${exercise.vikt}kg${
+      exercise.kommentar ? ": " + exercise.kommentar : ""
+    }`;
 
-        })
-
-        li.appendChild(editBtn);
-        logList.appendChild(li);
+    const editBtn = document.createElement("button");
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+    editBtn.addEventListener("click", () => {
+      currentEditId = exercise.ID; // ID  för "edit läge"
+      form.date.value = exercise.datum;
+      form.exercise.value = exercise.övning;
+      form.set.value = exercise.set;
+      form.reps.value = exercise.reps;
+      form.weight.value = exercise.vikt;
+      form.comment.value = exercise.kommentar || "";
     });
+
+    li.appendChild(editBtn);
+    logList.appendChild(li);
+  });
 }
-    /*
+/*
     const editBtn = document.createElement('button'); 
     editBtn.textContent = "<i class="fa-solid fa-pen"></i>";
     
@@ -96,9 +113,8 @@ function renderLogList() {
     });
  */
 
-
-// const removeBtn = document.createElement("button"); 
-// removeBtn.innerText = "X"; 
+// const removeBtn = document.createElement("button");
+// removeBtn.innerText = "X";
 // filter ID? -> För att hitta den övningen som ska tas bort
 // removeBtn.addEventListener("click", () => {
 //  logik
