aa0d60ce8aefa9c94985761db7047016ee2dbf24
Erik Jishammar <erikjishammar@hotmail.com>
2025-10-10 09:23:39 +0200

    Fix: make server serve index.html for all routes

--- server.ts
+++ server.ts
@@ -20,7 +20,7 @@ app.use('/', routesSessions);
 
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
